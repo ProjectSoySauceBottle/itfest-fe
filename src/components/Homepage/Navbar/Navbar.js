461e8a9ef917501e327c63ce25a69ad4cd9b6706
fauzanaziz0098 <fauzanaziz395@gmail.com>
2025-07-04 15:58:30 +0700

    feat: update navbar mobile autoclose

--- src/components/Homepage/Navbar/Navbar.js
+++ src/components/Homepage/Navbar/Navbar.js
@@ -5,94 +5,119 @@ import { CiSearch, CiShoppingCart } from "react-icons/ci";
 import { MdClose, MdMenu } from "react-icons/md";
 import { useDisclosure } from "@mantine/hooks";
 import { Drawer } from "@mantine/core";
+import { useRouter } from "next/navigation";
 
 export default function Navbar() {
   const [opened, { open, close }] = useDisclosure(false);
+  const router = useRouter();
 
+  const handleRoute = (path) => {
+    router.push(path);
+    setTimeout(() => close(), 200);
+  };
   return (
-    <nav className="bg-background/80 font-poppins fixed w-full z-50 border-b border-b-[#453423]">
-      <div className="flex justify-between py-[1rem] items-center px-[7%]">
-        {/* Logo */}
-        <Link href="/">
-          <div className="text-white font-extrabold text-xl italic">
-            SSB<span className="text-primary">Cafe.</span>
-          </div>
-        </Link>
+    <header>
+      <nav className="bg-background/80 font-poppins fixed w-full z-50 border-b border-b-[#453423]">
+        <div className="flex justify-between py-[1rem] items-center px-[7%]">
+          {/* Logo */}
+          <Link href="/">
+            <div className="text-white font-extrabold text-xl italic">
+              SSB<span className="text-primary">Cafe.</span>
+            </div>
+          </Link>
 
-        {/* Desktop menu */}
-        <div className="hidden md:flex space-x-5 items-center">
-          <Link href="#home">
-            <div className="navbar-menu-link">Home</div>
-          </Link>
-          <Link href="#about">
-            <div className="navbar-menu-link">Tentang Kami</div>
-          </Link>
-          <Link href="#menu">
-            <div className="navbar-menu-link">Menu</div>
-          </Link>
-          <Link href="#contact">
-            <div className="navbar-menu-link">Kontak</div>
-          </Link>
+          {/* Desktop menu */}
+          <div className="hidden md:flex space-x-5 items-center">
+            <Link href="#home">
+              <div className="navbar-menu-link">Home</div>
+            </Link>
+            <Link href="#about">
+              <div className="navbar-menu-link">Tentang Kami</div>
+            </Link>
+            <Link href="#menu">
+              <div className="navbar-menu-link">Menu</div>
+            </Link>
+            <Link href="#contact">
+              <div className="navbar-menu-link">Kontak</div>
+            </Link>
+          </div>
+
+          {/* Mobile menu button */}
+          <div className="flex items-center">
+            <div className="flex space-x-3">
+              <button
+                aria-label="Search"
+                className="text-white hover:text-primary focus:outline-none"
+              >
+                <CiSearch size="20" />
+              </button>
+
+              <button
+                aria-label="Shopping Cart"
+                className="text-white hover:text-primary focus:outline-none"
+              >
+                <CiShoppingCart size="20" />
+              </button>
+              <button
+                onClick={() => (opened ? close() : open())}
+                type="button"
+                className="md:hidden text-white hover:text-primary focus:outline-none"
+              >
+                <MdMenu size="20" />
+              </button>
+            </div>
+          </div>
         </div>
 
-        {/* Mobile menu button */}
-        <div className="flex items-center">
-          <div className="flex space-x-3">
+        {/* Mobile menu */}
+        <Drawer
+          opened={opened}
+          onClose={close}
+          position="right"
+          size="75%"
+          offset={5}
+          radius={10}
+          closeButtonProps={{
+            icon: <MdMenu size={20} stroke={1.5} />,
+          }}
+          className="md:hidden overflow-hidden z-50"
+        >
+          <Drawer.Body p="0" className="font-poppins font-normal p-3 space-y-1">
             <button
-              aria-label="Search"
-              className="text-white hover:text-primary focus:outline-none"
+              onClick={() => handleRoute("/#home")}
+              className="block w-full text-left"
             >
-              <CiSearch size="20" />
-            </button>
-
-            <button
-              aria-label="Shopping Cart"
-              className="text-white hover:text-primary focus:outline-none"
-            >
-              <CiShoppingCart size="20" />
+              <div className="navbar-menu-link font-normal p-3 rounded-md text-base">
+                Home
+              </div>
             </button>
             <button
-              onClick={() => (opened ? close() : open())}
-              type="button"
-              className="md:hidden text-white hover:text-primary focus:outline-none"
+              onClick={() => handleRoute("/#about")}
+              className="block w-full text-left"
             >
-              <MdMenu size="20" />
+              <div className="navbar-menu-link font-normal p-3 rounded-md text-base">
+                Tentang Kami
+              </div>
             </button>
-          </div>
-        </div>
-      </div>
-
-      {/* Mobile menu */}
-      <Drawer
-        opened={opened}
-        onClose={close}
-        position="right"
-        size="75%"
-        className="md:hidden overflow-hidden z-50"
-      >
-        <Drawer.Body p="0" className="font-poppins font-normal p-3 space-y-1">
-          <Link href="/#home">
-            <div className="navbar-menu-link font-normal p-3 rounded-md text-base">
-              Home
-            </div>
-          </Link>
-          <Link href="/#about">
-            <div className="navbar-menu-link font-normal p-3 rounded-md text-base">
-              Tentang Kami
-            </div>
-          </Link>
-          <Link href="/#menu">
-            <div className="navbar-menu-link font-normal p-3 rounded-md text-base">
-              Menu
-            </div>
-          </Link>
-          <Link href="/#contact">
-            <div className="navbar-menu-link font-normal p-3 rounded-md text-base">
-              Kontak
-            </div>
-          </Link>
-        </Drawer.Body>
-      </Drawer>
-    </nav>
+            <button
+              onClick={() => handleRoute("/#menu")}
+              className="block w-full text-left"
+            >
+              <div className="navbar-menu-link font-normal p-3 rounded-md text-base">
+                Menu
+              </div>
+            </button>
+            <button
+              onClick={() => handleRoute("/#contact")}
+              className="block w-full text-left"
+            >
+              <div className="navbar-menu-link font-normal p-3 rounded-md text-base">
+                Kontak
+              </div>
+            </button>
+          </Drawer.Body>
+        </Drawer>
+      </nav>
+    </header>
   );
 }
