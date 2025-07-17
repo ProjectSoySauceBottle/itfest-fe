4854f8fc70ae67cbe16812bae0ec2bc0723dad57
fauzanaziz0098 <fauzanaziz395@gmail.com>
2025-07-17 20:29:13 +0700

    style: text color in menu homepage

--- src/components/Homepage/Menu/Menu.js
+++ src/components/Homepage/Menu/Menu.js
@@ -58,57 +58,57 @@ export default function Menu() {
 
       <div className="menu-section flex flex-wrap justify-center gap-5 mt-10">
         <FadeIn delay={400}>
-          <div className="group relative flex flex-col w-64 h-96 text-muted justify-end shadow items-center rounded-md overflow-hidden space-y-2">
+          <div className="group relative flex flex-col w-64 h-96 text-background justify-end shadow items-center rounded-md overflow-hidden space-y-2">
             <img
               src="/assets/images/coffee.jpg"
               alt="Menu Coffee"
               className="absolute top-0 left-0 group-hover:scale-150 transition duration-300 w-full h-full object-cover -z-10"
             />
-            <h3 className="text-center md:-translate-y-30 group-hover:translate-y-0 transition duration-300">
+            <h3 className="text-center -translate-y-30 group-hover:translate-y-0 transition duration-300">
               Coffee
             </h3>
             <a
               href="/menu/coffee"
               target="_blank"
-              className="cursor-pointer text-sm active:underline md:translate-y-10 group-hover:translate-y-0 transition duration-300 mb-2 hover:underline"
+              className="cursor-pointer text-sm active:underline translate-y-10 group-hover:translate-y-0 transition duration-300 mb-2 hover:underline"
             >
               Lihat Semua
             </a>
           </div>
         </FadeIn>
         <FadeIn delay={500}>
-          <div className="group relative flex flex-col w-64 h-96 text-muted justify-end shadow items-center rounded-md overflow-hidden space-y-2">
+          <div className="group relative flex flex-col w-64 h-96 text-background justify-end shadow items-center rounded-md overflow-hidden space-y-2">
             <img
               src="/assets/images/tea.jpg"
               alt="Menu Coffee"
               className="absolute top-0 left-0 group-hover:scale-150 transition duration-300 w-full h-full object-cover -z-10"
             />
-            <h3 className="text-center md:-translate-y-30 group-hover:translate-y-0 transition duration-300">
+            <h3 className="text-center -translate-y-30 group-hover:translate-y-0 transition duration-300">
               Non-Coffee
             </h3>
             <a
               href="/menu/tea"
               target="_blank"
-              className="cursor-pointer text-sm active:underline md:translate-y-10 group-hover:translate-y-0 transition duration-300 mb-2 hover:underline"
+              className="cursor-pointer text-sm active:underline translate-y-10 group-hover:translate-y-0 transition duration-300 mb-2 hover:underline"
             >
               Lihat Semua
             </a>
           </div>
         </FadeIn>
         <FadeIn delay={600}>
-          <div className="group relative flex flex-col w-64 h-96 text-muted justify-end shadow items-center rounded-md overflow-hidden space-y-2">
+          <div className="group relative flex flex-col w-64 h-96 text-background justify-end shadow items-center rounded-md overflow-hidden space-y-2">
             <img
               src="/assets/images/croissant.jpg"
               alt="Menu Coffee"
               className="absolute top-0 left-0 group-hover:scale-150 transition duration-300 w-full h-full object-cover -z-10"
             />
-            <h3 className="text-center md:-translate-y-30 group-hover:translate-y-0 transition duration-300">
+            <h3 className="text-center -translate-y-30 group-hover:translate-y-0 transition duration-300">
               Snack
             </h3>
             <a
               href="/menu/snack"
               target="_blank"
-              className="cursor-pointer text-sm active:underline md:translate-y-10 group-hover:translate-y-0 transition duration-300 mb-2 hover:underline"
+              className="cursor-pointer text-sm active:underline translate-y-10 group-hover:translate-y-0 transition duration-300 mb-2 hover:underline"
             >
               Lihat Semua
             </a>
