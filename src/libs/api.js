59822d7eb6e4867316c27d8b926baa0211148f95
fauzanaziz0098 <fauzanaziz395@gmail.com>
2025-07-24 18:33:53 +0700

    chore: add authorization to api.js and useServerFetch

--- src/libs/api.js
+++ src/libs/api.js
@@ -1,11 +1,24 @@
 // lib/api.js
 import axios from "axios";
+import { getCookie } from "cookies-next";
 
 const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
+function getToken() {
+  return getCookie("token") || "";
+}
 const instance = axios.create({
   baseURL: BASE_URL,
-  //   withCredentials: true, // jika pakai cookie auth
+  // withCredentials: true,
+});
+
+instance.interceptors.request.use((config) => {
+  const token = getToken();
+  if (token) {
+    config.headers = config.headers ?? {};
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 export const apiGet = async (endpoint, config = {}) => {
@@ -26,9 +39,9 @@ export const apiPost = async (endpoint, body = {}, config = {}) => {
   }
 };
 
-export const apiPatch = async (endpoint, body = {}, config = {}) => {
+export const apiPut = async (endpoint, body = {}, config = {}) => {
   try {
-    const res = await instance.patch(endpoint, body, config);
+    const res = await instance.put(endpoint, body, config);
     return { data: res.data, error: null };
   } catch (error) {
     return { data: null, error };
--- src/libs/useServerFetch.js
+++ src/libs/useServerFetch.js
@@ -1,11 +1,25 @@
 // lib/serverFetch.js
 import axios from "axios";
+import { cookies } from "next/headers";
 
 const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 export const useServerFetch = async (endpoint, config = {}) => {
+  const cookieStore = cookies();
+  const token = cookieStore?.get("token")?.value;
+  console.log("token", token);
+
   try {
-    const res = await axios.get(`${BASE_URL}${endpoint}`, config);
+    const res = await axios.get(`${BASE_URL}${endpoint}`, {
+      ...config,
+      headers: {
+        ...config.headers,
+        Authorization: token ? `Bearer ${token}` : undefined,
+      },
+    });
+
+    console.log("ressss", endpoint, res.data);
+
     return { data: res.data, error: null };
   } catch (error) {
     return { data: null, error };
