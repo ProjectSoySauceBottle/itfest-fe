85ff754caee4396945f96264c438a42cd56bf634
fauzanaziz0098 <fauzanaziz395@gmail.com>
2025-07-24 22:32:31 +0700

    feat: add cookie to useClientFetch

--- src/hook/useClientFetch.js
+++ src/hook/useClientFetch.js
@@ -1,14 +1,26 @@
 "use client";
 import { useEffect, useState, useCallback } from "react";
 import axios from "axios";
+import { getCookie } from "cookies-next";
 
 const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+
+function getToken() {
+  return getCookie("token") || "";
+}
 
 export const useClientFetch = (endpoint, options = {}) => {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(options.initialLoading ?? true);
   const [error, setError] = useState(null);
 
+  const token = getToken();
+  const axios = require("axios").create({
+    baseURL: BASE_URL,
+    headers: {
+      Authorization: token ? `Bearer ${token}` : undefined,
+    },
+  });
   const fetchData = useCallback(async () => {
     if (!endpoint) return;
 
